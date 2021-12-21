db19a95af344f985aaaf15da71ebed9004801ed3
addhyanmalhotra <addhyanmalhotra@gmail.com>
2021-12-21 17:41:37 +0530

    add numbers

---
+++ fibbonacci/autograder_files/handin.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+	int n, next = 0, cur = 0, prev = 1, T;
+	scanf("%d", &T);
+	while(T--) {
+		scanf("%d", &n);
+		while(n--) {
+			next = cur + prev;
+			prev = cur;
+			cur = next;
+		}		
+		printf("%d\n", cur+1);
+	}
+	return 0;
+}
